c79cd5ffda0f2d8713fec433545b457652b3d162
Linas <linas404@protonmail.com>
2024-12-14 06:56:59 +0100

    - create project

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Linas on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
